fcef8734b87e9295bec489ed85d47df57d063b00
ysera34 <ycyoon34@gmail.com>
2020-04-12 14:32:33 +0900

    spring boot initialize for spring boot doc gs : Building an Application with Spring Boot

---
+++ starter/README.md
@@ -0,0 +1,1 @@
+### [Building an Application with Spring Boot](https://spring.io/guides/gs/spring-boot/)
---
+++ starter/src/test/kotlin/org/inframincer/spring/boot/starter/StarterApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.inframincer.spring.boot.starter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StarterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
