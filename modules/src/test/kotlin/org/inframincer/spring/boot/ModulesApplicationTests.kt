b43ab831c4cdc8d3eb8d23f1a8cddaf64233b25c
ysera34 <ycyoon34@gmail.com>
2020-11-11 00:45:49 +0900

    spring boot initialize for spring boot multi modules

---
+++ modules/src/test/kotlin/org/inframincer/spring/boot/ModulesApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.inframincer.spring.boot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ModulesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
